6b1f193d4615766b583d95f587144c7923eb85d3
Diego M-R <95539668+diegomendez40@users.noreply.github.com>
2024-06-15 03:19:16 +0200

    Updated testnode.py
    Updated testnode.py and test_textnode.py
    Added function split_nodes_delimiter and corresponding unittests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,13 +1,49 @@
 import unittest
 
-from textnode import TextNode
-
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", "bold", "url1")
         node2 = TextNode("This is a text node", "bold", "url1")
         self.assertEqual(node, node2)
+    
+    def test_split_nodes_bold(self):
+        node = TextNode("This is text with a **bold** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        print(new_nodes)
+        
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "bold")
+        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_split_nodes_italic(self):
+        node = TextNode("This is *italic* text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
+        
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "italic")
+        self.assertEqual(new_nodes[1].text_type, TextType.ITALIC)
+        self.assertEqual(new_nodes[2].text, " text")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_split_nodes_code(self):
+        node = TextNode("This is `code` text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "code")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " text")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,48 @@
+from htmlnode import LeafNode
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+        # import pdb; pdb.set_trace()
+        new_nodes = []
+        for old_node in old_nodes:
+            if old_node.text_type != TextType.TEXT:
+                new_nodes.append(old_node)
+            else:
+                segments = old_node.text.split(delimiter)
+                for i, segment in enumerate(segments):
+                    if i % 2 == 0:
+                        new_nodes.append(TextNode(segment, old_node.text_type))
+                    else:
+                        new_nodes.append(TextNode(segment, text_type))
+        return new_nodes
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href":text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src":text_node.url, "alt":text_node.text})
+    raise Exception("Unsupported TextNode type")
+
 class TextNode:
 
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
